4105179430efcfd934f4b2a4f624bbae67440980
Nathan McIntosh <Nathan.McIntosh@jhuapl.edu>
2021-11-04 09:04:33 -0400

    Added function for reading input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+/// is_choice_1 will give a user two choices, read their input from stdin, and return 
+/// true if they chose `choice1`, and false if `choice2`.
+fn is_choice_1(choice1: &str, choice2: &str) -> bool {
+    print!("{} (1) -- {} (2)?", choice1, choice2);
+    let mut input = String::new();
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => match input.as_str() {
+            "1" => true,
+            "2" => false,
+            _ => {
+                println!("Your input {} was not 1 or 2. Please try again", input);
+                is_choice_1(choice1, choice2)
+            }
+        }
+        Err(_) => {
+            println!("Could not read your input. Please try again");
+            is_choice_1(choice1, choice2)
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
